a21f44ebd3e03ae0940d82da2a68c64aeb89dc8c
han1202012 <13241153187@163.com>
2018-11-16 21:53:06 +0800

    第一次提交

---
+++ app/src/main/java/hanshuliang/com/ui_demos_4_csdn_blog/A_Measure_Layout_Demo_Activity.java
@@ -0,0 +1,6 @@
+package hanshuliang.com.ui_demos_4_csdn_blog;
+
+import android.app.Activity;
+
+public class A_Measure_Layout_Demo_Activity extends Activity {
+}
---
+++ app/src/main/java/hanshuliang/com/ui_demos_4_csdn_blog/B_Paint_Demo_Activity.java
@@ -0,0 +1,6 @@
+package hanshuliang.com.ui_demos_4_csdn_blog;
+
+import android.app.Activity;
+
+public class B_Paint_Demo_Activity extends Activity {
+}
