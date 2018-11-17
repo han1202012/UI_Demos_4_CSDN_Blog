f87cab898a36c5676ec342cad7295cd56bc4fca6
han1202012 <13241153187@163.com>
2018-11-17 11:20:47 +0800

    第一次提交

--- app/src/main/java/hanshuliang/com/ui_demos_4_csdn_blog/A_Measure_Layout_Demo_Activity.java
+++ app/src/main/java/hanshuliang/com/ui_demos_4_csdn_blog/A_Measure_Layout_Demo_Activity.java
@@ -1,7 +1,14 @@
 package hanshuliang.com.ui_demos_4_csdn_blog;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 
 public class A_Measure_Layout_Demo_Activity extends Activity {
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
+
